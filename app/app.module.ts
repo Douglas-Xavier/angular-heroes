24973e764dd5e710f7a6c0ec4f0b94e590b0bf14
Douglas Xavier <douglasxavier95@gmail.com>
2016-10-24 15:06:59 -0200

    Inicio da implementação do http, erro no loading.

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,36 +1,34 @@
-import { NgModule } from '@angular/core';
+import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-import { RouterModule } from '@angular/router';
-
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes.component';
-import { HeroDetailComponent } from './hero-detail.component';
-import { DashboardComponent } from './dashboard.component';
+import { FormsModule }   from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 
-import { HeroService } from "./hero.service";
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
-
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard.component';
+import { HeroesComponent }      from './heroes.component';
+import { HeroDetailComponent }  from './hero-detail.component';
+import { HeroService }          from './hero.service';
 
 @NgModule({
-    imports : [
-        BrowserModule,
-        FormsModule,
-        AppRoutingModule,
-        ],
-    declarations : [
-        AppComponent,
-        HeroesComponent,
-        HeroDetailComponent,
-        DashboardComponent
-    ],
-    bootstrap: [
-        AppComponent
-    ],
-    
-    providers: [HeroService],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
+    AppRoutingModule
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent,
+  ],
+  providers: [ HeroService ],
+  bootstrap: [ AppComponent ]
 })
-
-export class AppModule{}
+export class AppModule { }
